17d6e5134d847a2d75b0fe58fbb1163fd85badc5
uiwwnw Yoon <uiwwnwyoon@uiwwnwui-MacBook.local>
2018-09-27 22:18:15 +0900

    페이지버튼 추가 및 스코프 분리

--- austicky.js
+++ austicky.js
@@ -1,8 +1,5 @@
 ;(function() {
     var root = this;
-    var auSticky = {};
-    auSticky.exScrollTop;
-    auSticky.opt;
     var utils = (function() {
         var addEvnt = function(el, m, fn){
             el.addEventListener(m, fn);
@@ -10,55 +7,20 @@
         var removeEvnt = function(el, m, fn){
             el.removeEventListener(m, fn);
         };
-        var actSticky = function(scrollTop, direct, i) {
-            var _actSticky = function() {
-                var body = document.querySelector('body');
-                if (direct) {
-                    if(document.querySelector('[data-asticky="'+ Number(auSticky.idx)+'"]') === null) {
-                        var _clone = body.appendChild(ctr.clone);
-                        _clone.setAttribute('data-asticky', auSticky.idx);
-                        _clone.classList.add(auSticky.opt.activeClassName);
-                        ctr.el.setAttribute('data-margintop', ctr.marginTop);
-                        _clone.setAttribute('style', 'margin-top:' + ctr.marginTop + 'px;');
-                    }
-                } else {
-                    var _clone = document.querySelector('[data-asticky="'+ Number(auSticky.idx)+'"]');
-                    ctr.el.removeAttribute('data-margintop');
-                    (_clone !== null) && (body.removeChild(_clone));
-                }
-            };
-            auSticky.idx = i === undefined ? auSticky.idx:i;
-            var ctr = utils.ctr(auSticky.idx);
-            if((scrollTop + ctr.marginTop > ctr.top) && direct) {
-                (auSticky.elemList.length > auSticky.idx) && (_actSticky(), auSticky.idx++);
-                (auSticky.idx === auSticky.elemList.length) && (auSticky.idx = auSticky.elemList.length - 1);
-            };
-            if((scrollTop + ctr.marginTop <= ctr.top) && !direct) {
-                (auSticky.idx >= 0) && (_actSticky(), auSticky.idx--);
-                (auSticky.idx === -1) && (auSticky.idx = 0);
-            };
-        };
-        var scroll = function(){
-            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            auSticky.exScrollTop;
-            var direct = scrollTop > auSticky.exScrollTop;
-            if((auSticky.exScrollTop === undefined) && (scrollTop > 5)) {
-                direct = true;
-                for(var i = 0; i < auSticky.elemList.length;i++) {
-                    utils.actSticky(scrollTop, direct, i);
-                }
-            };
-            utils.actSticky(scrollTop, direct);
-            auSticky.exScrollTop = scrollTop;
-        };
-        var ctr = function(i) {
+        var ctr = function(elemList, i, option) {
             var ctr = {
-                el: auSticky.elemList[i],
-                top: auSticky.elemList[i].offsetTop,
-                marginTop: auSticky.elemList[i - 1] === undefined?0:Number(auSticky.elemList[i - 1].getAttribute('data-margintop')) + Number(auSticky.elemList[i - 1].offsetHeight),
-                clone: auSticky.elemList[i].cloneNode(true),
+                el: elemList[i],
+                top: option.bottom?elemList[i].offsetTop - window.innerHeight + elemList[i].offsetHeight:elemList[i].offsetTop,
+                margin: elemList[i - 1] === undefined?0:Number(elemList[i - 1].getAttribute('data-margin')) + Number(elemList[i - 1].offsetHeight),
+                clone: elemList[i].cloneNode(true)
             };
+            ctr.margin = option.bottom?-ctr.margin:ctr.margin;
             return ctr;
+        };
+        var scroll = function(scr) {
+            scr.top = window.pageYOffset || document.documentElement.scrollTop;
+            scr.direct = scr.top > scr.exTop;
+            return scr;
         };
         var opt = function(e) {
             var opt = {
@@ -68,16 +30,19 @@
             return opt;
         };
         var initElem = function(e) {
-            auSticky.elemNode = e;
-            auSticky.elemList = document.querySelectorAll(auSticky.elemNode).length === 0?false:document.querySelectorAll(auSticky.elemNode);
+            var elemNode = e;
+            var elemList = document.querySelectorAll(elemNode).length === 0?false:document.querySelectorAll(elemNode);
+            return elemList;
         };
-        var addElem = function(e) {
-            auSticky.elemNode = auSticky.elemNode + ', ' + e;
-            auSticky.elemList = document.querySelectorAll(auSticky.elemNode).length === 0?false:document.querySelectorAll(auSticky.elemNode);
+        var addElem = function(elemNode, e) {
+            var elemNode = elemNode + ', ' + e;
+            var elemList = document.querySelectorAll(elemNode).length === 0?false:document.querySelectorAll(elemNode);
+            return elemList;
         };
-        var removeElem = function(e) {
-            auSticky.elemNode = auSticky.elemNode.replace(e, '');
-            auSticky.elemList = document.querySelectorAll(auSticky.elemNode).length === 0?false:document.querySelectorAll(auSticky.elemNode);
+        var removeElem = function(elemNode, e) {
+            var elemNode = elemNode.replace(e, '');
+            var elemList = document.querySelectorAll(elemNode).length === 0?false:document.querySelectorAll(elemNode);
+            return elemList;
         };
         return {
             initElem: initElem,
@@ -85,21 +50,75 @@
             removeElem: removeElem,
             addEvnt: addEvnt,
             removeEvnt: removeEvnt,
-            scroll: scroll,
             ctr: ctr,
-            opt: opt,
-            actSticky: actSticky
+            scroll: scroll,
+            opt: opt
         }
     }());
-    var AuSticky = function(e, opt) {
-        auSticky.idx = 0;
-        auSticky.opt = utils.opt(opt);
-        utils.initElem(e);
-        utils.addEvnt(window, 'scroll', utils.scroll);
-        return {
-            addElem: utils.addElem
+    var auSticky = function(e, opt) {
+        auSticky.idx = auSticky.idx === undefined?0:auSticky.idx+=1;
+        var _idx = auSticky.idx;
+        var idx = 0;
+        var scr = {};
+        var option = utils.opt(opt);
+        var elemList = utils.initElem(e);
+        var actSticky = function(scrollTop, direct, i) {
+            var _actSticky = function() {
+                var body = document.querySelector('body');
+                if (direct) {
+                    if(document.querySelector('[data-asticky="' + _idx + '_' + idx + '"]') === null) {
+                        var _clone = body.appendChild(ctr.clone);
+                        _clone.setAttribute('data-asticky', _idx + '_' + idx);
+                        _clone.classList.add(option.activeClassName);
+                        ctr.el.setAttribute('data-margin', ctr.margin);
+                        _clone.setAttribute('style', 'margin-top:' + ctr.margin + 'px;');
+                        if(option.bottom) {
+                           body.setAttribute('style', 'margin-bottom:' + Number(ctr.el.offsetHeight - ctr.margin) + 'px;');
+                           ctr.el.setAttribute('data-margin', -ctr.margin);
+                           ctr.el.setAttribute('style', 'visibility: hidden;');
+                           _clone.setAttribute('style', 'margin-bottom:' + -ctr.margin + 'px;');
+                        };
+                    }
+                } else {
+                    var _clone = document.querySelector('[data-asticky="'+ _idx + '_' + idx + '"]');
+                    ctr.el.removeAttribute('data-margin');
+                    ctr.el.removeAttribute('style');
+                    (_clone !== null) && (body.removeChild(_clone));
+                    if(option.bottom) {
+                        var margin = ctr.margin === 0?'':'margin-bottom:' + -ctr.margin+'px;';
+                        body.setAttribute('style', margin);
+                     };
+                }
+            };
+            var _i = i === undefined ? idx:i;
+            var ctr = utils.ctr(elemList, _i, option);
+            if((scrollTop + ctr.margin > ctr.top) && direct) {
+                (elemList.length > _i) && (_actSticky(), _i++);
+                (_i === elemList.length) && (_i = elemList.length - 1);
+                idx = _i;
+            };
+            if((scrollTop + ctr.margin <= ctr.top) && !direct) {
+                (_i >= 0) && (_actSticky(), _i--);
+                (_i === -1) && (_i = 0);
+                idx = _i;
+            };
         };
+        var scroll = function(){
+            utils.scroll(scr);
+            if(scr.exTop === undefined) {
+                scr.direct = true;
+                for(var i = 0; i < elemList.length; i++) {
+                    actSticky(scr.top, scr.direct, i);
+                }
+            };
+            actSticky(scr.top, scr.direct);
+            scr.exTop = scr.top;
+        };
+        utils.addEvnt(window, 'scroll', scroll);
+        return {
+            elemList: elemList
+        }
     };
     
-    root.auSticky = AuSticky;
+    root.auSticky = auSticky;
 }());
